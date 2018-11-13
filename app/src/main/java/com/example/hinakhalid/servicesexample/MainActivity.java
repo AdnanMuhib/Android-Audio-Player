c7a7715a45c235bf763859f711bf825ebf2206f8
Adnan <adnan.muhib@rocketmail.com>
2018-11-14 01:26:31 +0500

    :heart_eyes: Playlist Creation, Play Next or Previous Song

--- app/src/main/java/com/example/hinakhalid/servicesexample/MainActivity.java
+++ app/src/main/java/com/example/hinakhalid/servicesexample/MainActivity.java
@@ -1,103 +1,50 @@
 package com.example.hinakhalid.servicesexample;
 
+import android.content.ClipData;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
-import android.os.ParcelFileDescriptor;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
 import java.io.File;
-import java.io.FileDescriptor;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
-    ArrayList<HashMap<String, String>> songsPlaylist;
+    ArrayList<Uri> playList;
+    int current_song_index;
+    int last_song_index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        File extStore = Environment.getExternalStorageDirectory();
-
-        String sdpath, sd0path;
-        sdpath="/storage/extSdCard/";
-        if(new File("/storage/extSdCard/").exists())
-        {
-            sdpath="/storage/extSdCard/";
-            Log.i("Sd Cardext Path",sdpath);
-        }
-        if(new File("/storage/sdcard1/").exists())
-        {
-            sdpath="/storage/sdcard1/";
-            Log.i("Sd Card1 Path",sdpath);
-        }
-        if(new File("/storage/usbcard1/").exists())
-        {
-            sdpath="/storage/usbcard1/";
-            Log.i("USB Path",sdpath);
-        }
-        if(new File("/storage/sdcard0/").exists())
-        {
-            sd0path="/storage/sdcard0/";
-            Log.i("Sd Card0 Path",sd0path);
-        }
-       songsPlaylist = new ArrayList<HashMap<String, String>>();
-      // getPlayList(sdpath);
-        if(songsPlaylist != null){
-            for(int i=0;i<songsPlaylist.size();i++){
-                String fileName=songsPlaylist.get(i).get("file_name");
-                String filePath=songsPlaylist.get(i).get("file_path");
-                //here you will get list of file name and file path that present in your device
-                Log.e("Songs"," name ="+fileName +" path = "+filePath);
-            }
-        }
-
+        //File extStore = Environment.getExternalStorageDirectory();
+        current_song_index = -1;
+        last_song_index = -1;
     }
-    ArrayList<HashMap<String,String>> getPlayList(String rootPath) {
-            try {
-                File rootFolder = new File(rootPath);
-                File[] files = rootFolder.listFiles(); //here you will get NPE if directory doesn't contains  any file,handle it like this.
-                for (File file : files) {
-                    String filname = file.getName();
-                    if (file.getName().endsWith(".mp3")){
-                        HashMap<String, String> song = new HashMap<>();
-                        song.put("file_path", file.getAbsolutePath());
-                        song.put("file_name", file.getName());
-                        songsPlaylist.add(song);
-                    }
-                    else if (file.isDirectory()) {
-                        if (getPlayList(file.getAbsolutePath()) != null) {
-                            //fileList.addAll(getPlayList(file.getAbsolutePath()));
-                            getPlayList(file.getAbsolutePath());
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                return songsPlaylist;
-            } catch (Exception e) {
-                return null;
-            }
-    }
+
     public  void OnBtnFolderSelect_Clicked(View view) {
             Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             intent.addCategory(Intent.CATEGORY_OPENABLE);
             intent.setType("audio/mpeg");
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         startActivityForResult(intent, 1);
     }
+    public  void OnBtnCreatePlaylist_Clicked(View view) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("audio/mpeg");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        startActivityForResult(intent, 2);
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -118,8 +65,42 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             break;
+            case 2:{
+                if (resultCode == RESULT_OK){
+                    //Uri uri = null;
+                    if (data != null) {
+                        playList = new ArrayList<>();
+                        ClipData clipData = data.getClipData();
+                        if (clipData != null) {
+                            for (int i = 0; i < clipData.getItemCount(); i++) {
+                                ClipData.Item item = clipData.getItemAt(i);
+                                Uri uri = item.getUri();
+                                playList.add(uri);
+                                //In case you need image's absolute path
+                                //String path= getRealPathFromURI(getActivity(), uri)
+                            }
+                        }
+                        //playList = (ArrayList) data.getData();
+                        //Log.i("PATH", "Uri: " + uri.toString());
+                    }
+                    Log.i("PATH","Address");
+                    try {
+                        if(playList.size() > 0){
+                            current_song_index = 0;
+                            last_song_index = playList.size() -1;
+                            Uri firstSong = playList.get(0);
+                            this.playMusic(firstSong);
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            break;
         }
     }
+
     public  void playMusic(Uri path) throws IOException {
         if(mediaPlayer!= null){
             mediaPlayer.pause();
@@ -134,7 +115,11 @@ public class MainActivity extends AppCompatActivity {
                 new MediaPlayer.OnCompletionListener() {
                     @Override
                     public void onCompletion(MediaPlayer mp) {
-                        playNextSong();
+                        try {
+                            playNextSong();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
         );
@@ -148,22 +133,29 @@ public class MainActivity extends AppCompatActivity {
         else {
             mediaPlayer.start();
         }
+
     }
 
 
-    public void btnPlayNext_Clicked(View view){
+    public void btnPlayNext_Clicked(View view) throws IOException {
         playNextSong();
     }
 
-    public void playNextSong(){
-
+    public void playNextSong() throws IOException {
+        if (current_song_index < last_song_index){
+            playMusic(playList.get(current_song_index+1));
+            current_song_index = current_song_index + 1;
+        }
     }
 
-    public void btnPlayPrevious_Clicked(View view){
+    public void btnPlayPrevious_Clicked(View view) throws IOException {
         playPreviousSong();
     }
 
-    public void playPreviousSong(){
-
+    public void playPreviousSong() throws IOException {
+        if (current_song_index > 0){
+            playMusic(playList.get(current_song_index-1));
+            current_song_index = current_song_index - 1;
+        }
     }
 }
