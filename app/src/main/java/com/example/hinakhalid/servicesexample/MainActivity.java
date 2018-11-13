9e96d71af3267b5a5e4ce1726a8926bbe5e1b41b
Adnan <adnan.muhib@rocketmail.com>
2018-11-14 01:58:09 +0500

    :100: Show Playlist Songs in Dialogue List View

--- app/src/main/java/com/example/hinakhalid/servicesexample/MainActivity.java
+++ app/src/main/java/com/example/hinakhalid/servicesexample/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.hinakhalid.servicesexample;
 
+import android.app.AlertDialog;
 import android.content.ClipData;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -130,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
         if (mediaPlayer != null && mediaPlayer.isPlaying()){
             mediaPlayer.pause();
         }
-        else {
+        else if(mediaPlayer != null) {
             mediaPlayer.start();
         }
 
@@ -158,4 +164,72 @@ public class MainActivity extends AppCompatActivity {
             current_song_index = current_song_index - 1;
         }
     }
+    public  void showPlayList(View view){
+        AlertDialog.Builder builderSingle = new AlertDialog.Builder(MainActivity.this);
+        //builderSingle.setIcon(R.drawable.ic_launcher);
+        builderSingle.setTitle("Select One Name:-");
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.select_dialog_singlechoice);
+        for (Uri u: playList) {
+            arrayAdapter.add(getFileNameFromUri(u));
+        }
+        //arrayAdapter.add("Hardik");
+        //arrayAdapter.add("Archit");
+        //arrayAdapter.add("Jignesh");
+        //arrayAdapter.add("Umang");
+        //arrayAdapter.add("Gatti");
+
+        builderSingle.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builderSingle.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String strName = arrayAdapter.getItem(which);
+                try {
+                    playMusic(playList.get(which));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+//                AlertDialog.Builder builderInner = new AlertDialog.Builder(MainActivity.this);
+//                builderInner.setMessage(strName);
+//                builderInner.setTitle("Your Selected Item is");
+//                builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog,int which) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//                builderInner.show();
+            }
+        });
+        builderSingle.show();
+    }
+    public String getFileNameFromUri(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
 }
